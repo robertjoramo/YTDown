509390556094fda1392f58c187a4484a8a299369
Robert Joramo <joramo.robertk@gmail.com>
2023-11-04 22:07:13 +0100

    Update ytDownloader.py

--- ytDownloader.py
+++ ytDownloader.py
@@ -1,12 +1,21 @@
 # Create virtual enviroment
-# python -m venv ytdown
-# ytdown\Scripts\activate
+# python -m venv venvYD
+# venvYD\Scripts\activate
 
 from pytube import YouTube
 from sys import argv
 
 #link = argv[1]
-yt = YouTube('https://www.youtube.com/watch?v=kCMJxOSiWrg')
+link = 'https://www.youtube.com/watch?v=LDU_Txk06tM'
+yt = YouTube(link)
+quality = 'get_lowest_resolution()'
+downloadFolder = 'E:\YT Downloads'
+
+
 
 print("Title: ", yt.title)
 print("View: ", yt.views)
+
+yd = yt.streams.get_lowest_resolution()
+
+yd.download(downloadFolder)
